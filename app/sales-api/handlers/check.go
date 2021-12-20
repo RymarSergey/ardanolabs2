2a2001f4b8d12364564074e0ad1dc33f9ffd7794
sergey <rymarsv@gmail.com>
2021-12-20 21:47:56 +0200

    add requet and response files to our web framework

--- app/sales-api/handlers/check.go
+++ app/sales-api/handlers/check.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
+	"github.com/RymarSergey/ardanolabs2/foundation/web"
 	"log"
 	"net/http"
 )
@@ -11,12 +11,12 @@ type check struct {
 	log *log.Logger
 }
 
-func (c check) readiness(context context.Context, w http.ResponseWriter, r *http.Request) error {
+func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
 	status := struct {
 		Status string
 	}{Status: "OK"}
 
 	log.Println(r, status)
-	return json.NewEncoder(w).Encode(status)
+	return web.Respond(ctx, w, status, http.StatusOK)
 }
